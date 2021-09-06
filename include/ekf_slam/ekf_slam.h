e2f71a167df879e45723f65d0d7da50a1c9b9ad3
Refstop <bhbhchoi@gmail.com>
2021-09-06 11:22:51 +0900

    integrate sub, fixing error

--- include/ekf_slam/ekf_slam.h
+++ include/ekf_slam/ekf_slam.h
@@ -8,7 +8,6 @@
 #include <message_filters/sync_policies/approximate_time.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h> 
 #include <geometry_msgs/PoseStamped.h>
-// #include <fiducial_msgs/FiducialTransformArray.h>
 #include <indoor_2d_nav/FiducialTransformArray_i2n.h>
 #include <ekf_slam_ros/Dead_reckoning.h>
 
@@ -16,15 +15,39 @@ using namespace std;
 using namespace Eigen;
 using namespace message_filters;
 
+typedef struct observation {
+    int marker_id;
+    Vector3f point;
+} obsv;
+
 class ekf_slam {
 public:
-    ekf_slam(MatrixXf& X, MatrixXf& P, const MatrixXf& Z, const MatrixXf& U, const MatrixXf& marker_ids);
+    ekf_slam();
     ~ekf_slam();
     void prediction();
     void correction();
-    MatrixXf get_X() const;
-    MatrixXf get_P() const;
+    void poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
+    void arucoCallback(const indoor_2d_nav::FiducialTransformArray_i2n::ConstPtr& msg);
+    void encoderCallback(const ekf_slam_ros::Dead_reckoning::ConstPtr& msg);
 private:
+    /*
+        X: robot's pose(map-base_footprint): (x, y, phi)
+        P: robot's covariance: diag(Px, Py, Pphi)
+        Z: robot's observation(camera-marker): (x1, y1, phi1, x2, y2, phi2, x3, y3, phi3, ...)
+        U: robot's movement(meter, encoder output): (d_l, d_r)
+        marker_ids: ID of detected markers
+    */
     MatrixXf X_previous, Covariance, X_hat, S_hat, X_current, S_current, Z, U, marker_ids;
+    Vector2f encoder;
+    obsv* sorted;
     MatrixXf pose2rphi(const MatrixXf& pose);
+    ros::NodeHandle nh;
+    ros::Subscriber pose_sub;
+    ros::Subscriber aruco_sub;
+    ros::Subscriber encoder_sub;
+    ros::Publisher result_pub;
+    void merge(obsv list[], int left, int mid, int right);
+    void merge_sort(obsv list[], int left, int right);
+    void obsv_sort(obsv obsv_arr[], int detected_count);
+    void init_landmark();
 };
--- src/ekf_slam.cpp
+++ src/ekf_slam.cpp
@@ -1,46 +1,12 @@
 #include <ekf_slam/ekf_slam.h>
 
-ekf_slam::ekf_slam(
-    MatrixXf& X, // robot's pose(map-base_footprint): (x, y, phi)
-    MatrixXf& P, // robot's covariance: diag(Px, Py, Pphi)
-    const MatrixXf& Z, // robot's observation(camera-marker): (x1, y1, phi1, x2, y2, phi2, x3, y3, phi3, ...)
-    const MatrixXf& U, // robot's movement(meter, encoder output): (d_l, d_r)
-    const MatrixXf& marker_ids // ID of detected markers
-    ) :U(U), marker_ids(marker_ids) {
-    int max_id = 0, landmark_size = 0;
-    for(int i = 0; i < marker_ids.size(); i++) {
-        if(max_id < marker_ids(i)) max_id = marker_ids(i);
-    }
-    landmark_size = max_id + 1;
-    cout<<"X:"<<endl<<X<<endl<<endl;
-    if(landmark_size > X.size()/3 - 1) {
-        cout<<"landmark_size:"<<endl<<landmark_size<<endl<<endl;
-        cout<<"X.size()/3 - 1:"<<endl<<X.size()/3 - 1<<endl<<endl;
-        MatrixXf X_temp = X, P_temp = P;
-        X.resize(3 + 3*landmark_size, 1);
-        P.resize(3 + 3*landmark_size, 3 + 3*landmark_size);
-        int add_size = landmark_size - (X_temp.size()/3 - 1);
-        X << X_temp, MatrixXf::Zero(3*add_size, 1);
-        P << P_temp, MatrixXf::Zero(3, 3*add_size), MatrixXf::Zero(3*add_size, 3), 100*MatrixXf::Identity(3*add_size, 3*add_size);
-    }
-    int landmark_n = 0;
-    
-    for(int i = 0; i < marker_ids.size(); i++) {
-        landmark_n = 3*(marker_ids(i)+1);
-        cout<<"landmark_n:"<<endl<<landmark_n<<endl<<endl;
-        if(X(landmark_n) == 0 && X(landmark_n+1) == 0 && X(landmark_n+2) == 0) {
-            X(landmark_n) = Z(3*i);
-            X(landmark_n+1) = Z(3*i+1);
-            X(landmark_n+2) = Z(3*i+2);
-        }
-        
-    }
-    // X(0) += 0.06;
-    X_previous = X; // 3+3n*1
-    Covariance = P; // 3+3n*1
-    cout<<"X_previous:"<<endl<<X_previous<<endl<<endl;
-    cout<<"Covariance:"<<endl<<Covariance<<endl<<endl;
-    this->Z = pose2rphi(Z); //2n*1
+ekf_slam::ekf_slam() {
+    X_previous.resize(1,1);
+    Covariance.resize(1,1);
+    pose_sub = nh.subscribe("pose", 10, &ekf_slam::poseCallback, this);
+    aruco_sub = nh.subscribe("fiducial_transforms", 10, &ekf_slam::arucoCallback, this);
+    encoder_sub = nh.subscribe("Dead_reckoning", 10, &ekf_slam::encoderCallback, this);
+    result_pub = nh.advertise<geometry_msgs::PoseStamped>("result_pose", 10);
 }
 
 ekf_slam::~ekf_slam() {
@@ -59,7 +25,7 @@ void ekf_slam::prediction() {
     X_previous(0) += dx;
     X_previous(1) += dy;
     X_previous(2) += d_theta;
-    cout<<"dx, dy, d_theta:"<<endl<<dx<<dy<<d_theta<<endl<<endl;
+    cout<<"dx, dy, d_theta:"<<endl<<dx<<", "<<dy<<", "<<d_theta<<endl<<endl;
     X_hat = X_previous;
     cout<<"X_hat:"<<endl<<X_hat<<endl<<endl;
     
@@ -85,94 +51,204 @@ void ekf_slam::prediction() {
 }
 
 void ekf_slam::correction() {
-    MatrixXf delta(Z.rows()/2*3, 1), Q(Z.rows(), Z.rows()), H_low(Z.rows(), 6); //2n*1, 2n*2n, 2n*6
-    Q << 0.05*MatrixXf::Identity(Z.rows(), Z.rows()); // 2n*2n
+    MatrixXf Q(2,2), H_low(2,6), F(6,S_hat.rows()), Z_hat, H, K, X_current, S_current; //3*1, 2*2, 2*6, 6*3+3n
+    Vector3f delta(3,1);
+    float q;
+    Q << pow(0.05, 2)*MatrixXf::Identity(2,2); // 2*2
 
     // range bearing observation model
     // extract tf corrected with ID, delta = true_landmark - X_hat
     for(int i = 0; i < marker_ids.size(); i++) {
-        delta(3*i) = X_hat(3*(marker_ids(i)+1)) - X_hat(0);
-        delta(3*i+1) = X_hat(3*(marker_ids(i)+1)+1) - X_hat(1);
-        delta(3*i+2) = X_hat(3*(marker_ids(i)+1)+2) - X_hat(2);
-    }
+        delta = X_hat.block<3,1>(3*(marker_ids(i) + 1),0) - X_hat.block<3,1>(0,0);
+        // Z_hat
+        Z_hat = pose2rphi(delta);
+        q = delta.block<2,1>(0,0).dot(delta.block<2,1>(0,0));
+        // H
+        H_low <<
+        -delta(0)/sqrt(q), -delta(1)/sqrt(q), 0, delta(0)/sqrt(q), delta(1)/sqrt(q), 0,
+        delta(1)/q, -delta(0)/q, -1, -delta(1)/q, delta(0)/q,  0;
+        // homogeneous F
+        F <<
+        MatrixXf::Identity(3, 3), MatrixXf::Zero(3, F.cols()-3),
+        MatrixXf::Zero(3, 3*(marker_ids(i)+1)), MatrixXf::Identity(3, 3), MatrixXf::Zero(3, F.cols()-3*(marker_ids(i)+1)-3); // 6*3+3n
+        H = H_low*F; // 2*6 x 6*3+3n = 2*3+3
 
-    // Z_hat
-    auto Z_hat = pose2rphi(delta);
-
-    // H
-    float * q = new float[delta.size()/3];
-    for(int i = 0; i < delta.size()/3; i++) {
-        Vector2f delta_i;
-        delta_i << delta(3*i), delta(3*i+1);
-        q[i] = delta_i.dot(delta_i);
-    }
-    for(int i = 0; i < Z.rows()/2; i++) {
-        H_low(2*i, 0) = -delta(3*i)/sqrt(q[i]);
-        H_low(2*i, 1) = -delta(3*i+1)/sqrt(q[i]);
-        H_low(2*i, 2) = 0;
-        H_low(2*i, 3) = delta(3*i)/sqrt(q[i]);
-        H_low(2*i, 4) = delta(3*i+1)/sqrt(q[i]);
-        H_low(2*i, 5) = 0;
-
-        H_low(2*i+1, 0) = delta(3*i+1)/q[i]; 
-        H_low(2*i+1, 1) = -delta(3*i)/q[i]; 
-        H_low(2*i+1, 2) = -1;
-        H_low(2*i+1, 3) = -delta(3*i+1)/q[i]; 
-        H_low(2*i+1, 4) = delta(3*i)/q[i]; 
-        H_low(2*i+1, 5) = 0;
-    }
-
-    // homogeneous F
-    MatrixXf F(H_low.cols(), S_hat.rows()); // 6*3+3n
-    F << MatrixXf::Identity(3, 3), MatrixXf::Zero(3, S_hat.rows()-3), MatrixXf::Zero(3, 3), MatrixXf::Zero(3, S_hat.rows()-3); // 6*3+3n
+        cout<<"----------marker "<<i<<" correction-----------"<<endl<<endl;
+        cout<<"X_hat:"<<endl<<X_hat<<endl<<endl;
+        cout<<"S_hat:"<<endl<<S_hat<<endl<<endl;
+        cout<<"Z:"<<endl<<Z<<endl<<endl;
     
-    for(int i = 0; i < marker_ids.size(); i++) {
-        F.block<3,3>(3, 3*(marker_ids(i)+1)) = MatrixXf::Identity(3, 3);
-    }
-    auto H = H_low*F; // 2n*6 x 6*3+3n = 2n*3+3n
-
-    // correction step
-    auto K = S_hat*H.transpose()*(H*S_hat*H.transpose() + Q).inverse(); // K: 3+3n*3+3n x 3+3n*2n x (2n*3+3n x 3+3n*3+3n x 3+3n*2n + 2n*2n) = 3+3n*2n
-    X_current = X_hat + K*(Z - Z_hat); // X_current: 3+3n*1 + 3+3n*2n x (2n*1 - 2n*1) = 3+3n*1
-    S_current = S_hat - K*H*S_hat; // 3+3n*3+3n - 3+3n*2n x 2n*3+3n x 3+3n*3+3n = 3+3n*3+3n
-
-    cout<<"X_hat:"<<endl<<X_hat<<endl<<endl;
-    cout<<"S_hat:"<<endl<<S_hat<<endl<<endl;
-
-    cout<<"Z:"<<endl<<Z<<endl<<endl;
-
-    cout<<"delta:"<<endl<<delta<<endl<<endl;
-    cout<<"Z_hat:"<<endl<<Z_hat<<endl<<endl;
-    cout<<"Z - Z_hat:"<<endl<<Z - Z_hat<<endl<<endl;
-
-    cout<<"q:"<<endl<<q[0]<<endl<<q[1]<<endl<<endl;
-    cout<<"H_low:"<<endl<<H_low<<endl<<endl;
-    cout<<"F:"<<endl<<F<<endl<<endl;
-    cout<<"H:"<<endl<<H<<endl<<endl;
-    cout<<"Q:"<<endl<<Q<<endl<<endl;
+        cout<<"delta:"<<endl<<delta<<endl<<endl;
+        cout<<"q:"<<endl<<q<<endl<<endl;
+        cout<<"Z_hat:"<<endl<<Z_hat<<endl<<endl;
+        cout<<"Z - Z_hat:"<<endl<<Z.block<2,1>(2*i,0) - Z_hat<<endl<<endl;
     
-    cout<<"K:"<<endl<<K<<endl<<endl;
-    cout<<"X_current:"<<endl<<X_current<<endl<<endl;
-    cout<<"S_current:"<<endl<<S_current<<endl<<endl;
-
-    // X_current(0) -= 0.06;
-    delete q;
+        cout<<"H_low:"<<endl<<H_low<<endl<<endl;
+        cout<<"F:"<<endl<<F<<endl<<endl;
+        cout<<"H:"<<endl<<H<<endl<<endl;
+        
+        
+        // correction step
+        K = S_hat*H.transpose()*(H*S_hat*H.transpose() + Q).inverse(); // K: 3+3n*3+3n x 3+3n*2 x (2*3+3n x 3+3n*3+3n x 3+3n*2 + 2*2) = 3+3n*2
+        X_hat = X_hat + K*(Z.block<2,1>(2*i,0) - Z_hat); // X_current: 3+3n*1 + 3+3n*2 x (2*1 - 2*1) = 3+3n*1
+        S_hat = S_hat - K*H*S_hat; // 3+3n*3+3n - 3+3n*2 x 2*3+3n x 3+3n*3+3n = 3+3n*3+3n
+        X_hat(0) = X_hat(0) - 0.06;
+        cout<<"K:"<<endl<<K<<endl<<endl;
+        cout<<"X_current:"<<endl<<X_hat<<endl<<endl;
+        cout<<"S_current:"<<endl<<S_hat<<endl<<endl;
+    }
+    X_current = X_hat.block<3,1>(0,0);
+    S_current = S_hat.block<3,3>(0,0);
+    
+    geometry_msgs::PoseStamped result_pose;
+    result_pose.header.frame_id = "/map";
+    result_pose.header.stamp = ros::Time::now();
+    result_pose.pose.position.x = X_current(0);
+    result_pose.pose.position.y = X_current(1);
+    result_pose.pose.position.z = result_pose.pose.orientation.x = result_pose.pose.orientation.y = 0;
+    result_pose.pose.orientation.z = sin(X_current(2)/2);
+    result_pose.pose.orientation.w = cos(X_current(2)/2);
+    result_pub.publish(result_pose);
+    ROS_INFO("result: %f, %f, %f\n", X_current(0), X_current(1), X_current(2));
 }
 
 MatrixXf ekf_slam::pose2rphi(const MatrixXf& pose) { //pose: (x1, y1, phi1, x2, y2, phi2, x3, y3, phi3, ...)
     MatrixXf observation(pose.size()/3*2, 1);
     for(int i = 0; i < pose.size()/3; i++) {
         observation(2*i) = sqrt(pow(pose(3*i), 2) + pow(pose(3*i+1), 2));
-        // observation(2*i+1) = pose(3*i+2);
         observation(2*i+1) = atan2(pose(3*i+1),pose(3*i)) - X_previous(2);
     }
     return observation; // result: (r1, phi2, r2, phi2, ...)
 }
 
-MatrixXf ekf_slam::get_X() const{
-    return X_current;
+void ekf_slam::merge(obsv list[], int left, int mid, int right) {
+  int i, j, k, l;
+  i = left;
+  j = mid + 1;
+  k = left;
+
+  while(i <= mid && j <= right) {
+    if(list[i].marker_id <= list[j].marker_id)
+      sorted[k++] = list[i++];
+    else
+      sorted[k++] = list[j++];
+  }
+
+  if(i > mid) {
+    for(l = j; l <= right; l++)
+      sorted[k++] = list[l];
+  }
+ 
+  else {
+    for(l = i; l <= mid; l++)
+      sorted[k++] = list[l];
+  }
+
+  for(l = left; l <= right; l++) {
+    list[l] = sorted[l];
+  }
 }
 
-MatrixXf ekf_slam::get_P() const {
-    return S_current;
+void ekf_slam::merge_sort(obsv list[], int left, int right) {
+  int mid;
+
+  if(left < right) {
+    mid = (left + right) / 2;
+    merge_sort(list, left, mid); 
+    merge_sort(list, mid+1, right); 
+    merge(list, left, mid, right); 
+  }
 }
+
+void ekf_slam::obsv_sort(obsv obsv_arr[], int detected_count) {
+  sorted = new obsv[detected_count];
+  merge_sort(obsv_arr, 0, detected_count-1);
+}
+
+void ekf_slam::init_landmark() {
+    int max_id = 0, landmark_size = 0;
+    for(int i = 0; i < marker_ids.size(); i++) {
+        if(max_id < marker_ids(i)) max_id = marker_ids(i);
+    }
+    landmark_size = max_id + 1;
+    cout<<"X_previous:"<<endl<<X_previous<<endl<<endl;
+    if(landmark_size > X_previous.size()/3 - 1) {
+        cout<<"landmark_size:"<<endl<<landmark_size<<endl<<endl;
+        cout<<"X_previous.size()/3 - 1:"<<endl<<X_previous.size()/3 - 1<<endl<<endl;
+        MatrixXf X_temp = X_previous, P_temp = Covariance;
+        X_previous.resize(3 + 3*landmark_size, 1);
+        Covariance.resize(3 + 3*landmark_size, 3 + 3*landmark_size);
+        int add_size = landmark_size - (X_temp.size()/3 - 1);
+        X_previous << X_temp, MatrixXf::Zero(3*add_size, 1);
+        Covariance << P_temp, MatrixXf::Zero(3, 3*add_size), MatrixXf::Zero(3*add_size, 3), 100*MatrixXf::Identity(3*add_size, 3*add_size);
+    }
+    int landmark_n = 0;
+    for(int i = 0; i < marker_ids.size(); i++) {
+        landmark_n = 3*(marker_ids(i)+1);
+        cout<<"landmark_n:"<<endl<<landmark_n<<endl<<endl;
+        if(X_previous(landmark_n) == 0 && X_previous(landmark_n+1) == 0 && X_previous(landmark_n+2) == 0) {
+            X_previous(landmark_n) = X_previous(0) + Z(3*i);
+            X_previous(landmark_n+1) = X_previous(1) + Z(3*i+1);
+            X_previous(landmark_n+2) = X_previous(2) + Z(3*i+2);
+        }
+    }
+}
+
+
+// subscribe robot's pose from topic "/pose" (x, y, phi)
+void ekf_slam::poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) { //map-base_footprint
+    if(X_previous.size() != 1) return;
+    cout<<"pose set"<<endl;
+    X_previous.resize(3,1);
+    Covariance.resize(3,3);
+    // X << msg->pose.pose.position.x, msg->pose.pose.position.y,
+    // 2*atan2(msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+    // P << 
+    // msg->pose.covariance[0], msg->pose.covariance[1], 0,
+    // msg->pose.covariance[6], msg->pose.covariance[7], 0,
+    // 0, 0, msg->pose.covariance[35];
+    
+    // hardcode
+    X_previous << 0, 0, 0;
+    Covariance << 
+    1, 0, 0,
+    0, 1, 0,
+    0, 0, 1;
+}
+
+// subscribe measurement tf_camera_marker topic "/fiducial_transforms" (x, y, phi) from aruco marker
+void ekf_slam::arucoCallback(const indoor_2d_nav::FiducialTransformArray_i2n::ConstPtr& msg) {
+    MatrixXf Z_;
+    auto observed_markers = msg->transforms;
+    int detected_count = msg->detected_count;
+    obsv* obsv_arr = new obsv[detected_count];
+    marker_ids.resize(detected_count, 1);
+    Z_.resize(3*detected_count, 1);
+    
+    for(int i = 0; i < detected_count; i++) {
+        obsv_arr[i].point <<
+        observed_markers[i].transform.translation.z, -observed_markers[i].transform.translation.x,
+        2*atan2(observed_markers[i].transform.rotation.z, observed_markers[i].transform.rotation.w);
+        obsv_arr[i].marker_id = observed_markers[i].fiducial_id;
+    }
+    obsv_sort(obsv_arr, detected_count);
+    for(int i = 0; i < detected_count; i++) {
+        Z_(3*i) = obsv_arr[i].point(0);
+        Z_(3*i+1) = obsv_arr[i].point(1);
+        Z_(3*i+2) = obsv_arr[i].point(2);
+        marker_ids(i) = obsv_arr[i].marker_id;
+    }
+    Z = pose2rphi(Z_); //2n*1
+    init_landmark();
+    X_previous(0) += 0.06;
+    cout<<"Z_init:"<<endl<<Z<<endl<<endl;
+    cout<<"marker_ids:"<<endl<<marker_ids<<endl<<endl;
+    prediction();
+    correction();
+}
+
+void ekf_slam::encoderCallback(const ekf_slam_ros::Dead_reckoning::ConstPtr& msg) {
+  encoder(0) = msg->d_l;
+  encoder(1) = msg->d_r;
+}
+
